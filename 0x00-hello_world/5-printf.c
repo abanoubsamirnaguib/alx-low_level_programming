564adb871c52f3b3a006de00100d9bc8c81bb4de
abanoubsamirnaguib <abanoubsamir@gmail.com>
2023-08-10 18:07:01 -0700

    first commit

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - entery point
+ *
+ * Description: Write a C program that prints
+ *
+ * Return: the value 0 (succes)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
