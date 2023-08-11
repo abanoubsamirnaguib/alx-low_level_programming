c3a2e6773aa7686b2392dc7fc61ac23ad6b8031f
abanoubsamirnaguib <abanoubsamir@gmail.com>
2023-08-10 17:49:33 -0700

    first commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,15 @@
 #include<stdio.h>
-int main(void){
+
+/**
+ * main - entery point 
+ *
+ * Description: Write a C program that prints 
+ *
+ * Return: the value 0 (succes)
+ */
+
+int main(void)
+{
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
-	}
+}
