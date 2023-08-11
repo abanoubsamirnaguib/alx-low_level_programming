8bcfd58d9f93fc63915e6845128988eb66f84f43
abanoubsamirnaguib <abanoubsamir@gmail.com>
2023-08-10 18:55:20 -0700

    first commit

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - entery point
+ *
+ * Description: Write a C program that prints without printf or puts
+ *
+ * Return: the value 1 (unsucces)
+ */
+int main(void)
+{
+	char qou[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	write(1, quo, sizeof(qou));
+	return (1);
+}
