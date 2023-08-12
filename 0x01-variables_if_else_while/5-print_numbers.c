195d316a298917146e72e6d0b6180a17cc75f6d8
abanoubsamirnaguib <abanoubsamir@gmail.com>
2023-08-11 20:12:25 -0700

    first commit

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: false or true validation
+ *
+ * Return: 0 (success)
+ *
+ */
+
+int main(void)
+{
+	int digit = 0;
+
+	while (digit <= 9)
+	{
+		printf("%i", digit);
+		digit++;
+	}
+	printf('\n');
+
+	return (0);
+}
