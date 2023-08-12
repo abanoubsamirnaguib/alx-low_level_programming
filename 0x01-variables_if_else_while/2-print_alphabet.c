a56a3b60a3bab04dbf44b09783607c43c3aaa4f7
abanoubsamirnaguib <abanoubsamir@gmail.com>
2023-08-11 19:43:00 -0700

    first commit

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,23 +1,21 @@
 #include <stdio.h>
-
 /**
  * main - entry point
  *
  * Description: false or true validation
  *
  * Return: 0 (success)
- *
  */
 
 int main(void)
 {
 	char ch = 'a';
+
 	while (ch <= 'z')
 	{
 		putchar(ch);
 		ch++;
 	}
 	putchar('\n');
-
-        return (0);
+	return (0);
 }
