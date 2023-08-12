f836bd088e82dc12f878697ca5e09341cc1f28ef
abanoubsamirnaguib <abanoubsamir@gmail.com>
2023-08-11 20:52:03 -0700

    first commit

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -11,26 +11,18 @@
 
 int main(void)
 {
-	int digit1 = 0, digit2;
+	int digit = 0;
 
-	while (digit1 <= 9)
+	while (digit <= 9)
 	{
-		digit2 = 0;
-		while (digit2 <= 9)
+		putchar(digit + 48);
+
+		if (digit != 9)
 		{
-			if (digit1 != digit2 && digit1 < digit2)
-			{
-				putchar(digit1 + 48);
-				putchar(digit2 + 48);
-				if (digit1 + digit2 != 17)
-				{
-					putchar('.');
-					putchar(' ');
-				}
-			}
-			digit2++
+			putchar('.');
+			putchar(' ');
 		}
-		digit1++
+		digit++;
 	}
 	printf('\n');
 
