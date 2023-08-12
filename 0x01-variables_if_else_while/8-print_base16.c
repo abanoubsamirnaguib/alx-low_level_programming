c8e3404a5106f79f6993e7dfae5021f1ddbf8ebf
abanoubsamirnaguib <abanoubsamir@gmail.com>
2023-08-11 20:31:41 -0700

    first commit

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: false or true validation
+ *
+ * Return: 0 (success)
+ *
+ */
+
+int main(void)
+{
+	int digit = 48;
+
+	while (digit <= 102)
+	{
+		putchar(digit);
+
+			if (digit == 57)
+				digit += 39;
+		digit++;
+	}
+	printf('\n');
+
+	return (0);
+}
