1b69aed789b10c82f862957a0d43418c6a0263f0
abanoubsamirnaguib <abanoubsamir@gmail.com>
2023-08-11 19:59:39 -0700

    first commit

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: false or true validation
+ *
+ * Return: 0 (success)
+ *
+ */
+
+int main(void)
+{
+	char ch = 'a';
+	char ch = 'A';
+
+	/*print a - z */
+	while (ch <= 'z')
+	{
+		putchar(ch);
+		ch++;
+	}
+
+	/*print A - Z */
+	while (ch <= 'Z')
+	{
+		putchar(ch);
+		ch++;
+	}
+	putchar('\n');
+	return (0);
+}
