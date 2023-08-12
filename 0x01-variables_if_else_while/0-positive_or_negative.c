5aa0372ffe06a664ebecfb68fa829695ed218ec7
abanoubsamirnaguib <abanoubsamir@gmail.com>
2023-08-11 19:04:06 -0700

    first commit

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -18,10 +18,10 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	if (n > 0)
-		printf("%f is postive\n", n);
+		printf("%i is postive\n", n);
 	else if (n == 0)
-		printf("%f is zero\n", n);
+		printf("%i is zero\n", n);
 	else
-		printf("%f is negative\n", n);
+		printf("%i is negative\n", n);
 	return (0);
 }
