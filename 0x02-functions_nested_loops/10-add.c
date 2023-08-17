14376b448f05d0f3fa245945bf52602fe1d00b85
abanoubsamirnaguib <abanoubsamir@gmail.com>
2023-08-16 18:52:04 -0700

    c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * add - functon to print n int
+ * @a: first to print
+ * @b: second to print
+ *
+ * Description: print
+ *
+ * Return: 0 (success)
+ *
+ */
+
+int add(int a, int b)
+{
+	int sum = a + b;
+
+	return (sum);
+}
