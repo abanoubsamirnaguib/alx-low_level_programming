abc45fc8a1ab8b51a7afd000f1fea34d9295fb5a
abanoubsamirnaguib <abanoubsamir@gmail.com>
2023-08-16 18:17:22 -0700

    c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _abs - functon to print n int
+ * @n: parameter to print
+ *
+ * Description: print
+ *
+ * Return: 0 (success)
+ *
+ */
+
+int _abs(int n)
+{
+	if (n < 0)
+		n = (-1) * n;
+	return (n);
+}
