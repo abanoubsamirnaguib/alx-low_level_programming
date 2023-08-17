3d2c1ba82f1eaa42ea12dc321588c71dd1219677
abanoubsamirnaguib <abanoubsamir@gmail.com>
2023-08-16 19:07:42 -0700

    j

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -15,14 +15,10 @@ void print_to_98(int n)
 	int count;
 
 	if (n > 98)
-	{
 		for (count = n; count > 98; count--)
-		{
 			printf("%d. ", count);
-		}
-		else
+	else
 			for (count = n; count < 98; count++)
-				printf("d. ", count);
-		printf("98\n");
-	}
+				printf("%d. ", count);
+	printf("98\n");
 }
