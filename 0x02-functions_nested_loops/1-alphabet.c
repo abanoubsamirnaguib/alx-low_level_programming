5f198d6fd19a01985c69cbe03f0cf233355510e4
abanoubsamirnaguib <abanoubsamir@gmail.com>
2023-08-16 17:36:17 -0700

    c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * main - entry point
+ *
+ * Description: false or true validation
+ *
+ * Return: 0 (success)
+ *
+ */
+
+void print_alphabet(void)
+{
+	int ch;
+
+	/*print a - z */
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		_putchar(sh[ch]);
+	}
+	_putchar('\n');
+}
